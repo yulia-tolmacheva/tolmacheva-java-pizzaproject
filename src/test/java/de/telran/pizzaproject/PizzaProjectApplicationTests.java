bf87beeb770baeedf79a0bb0420c88e3ca594bdd
juli <julietatolm@gmail.com>
2023-05-16 19:49:26 +0200

    initial commit

---
+++ src/test/java/de/telran/pizzaproject/PizzaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.pizzaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
