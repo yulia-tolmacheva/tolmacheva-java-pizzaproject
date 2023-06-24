d21923599d03c063cc5612daf317b1d60262af2a
juli <julietatolm@gmail.com>
2023-06-24 13:16:06 +0200

    files update

--- src/test/java/de/telran/pizzaproject/PizzaProjectApplicationTests.java
+++ src/test/java/de/telran/pizzaproject/PizzaProjectApplicationTests.java
@@ -1,10 +1,8 @@
 package de.telran.pizzaproject;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
 @SpringBootTest
 class PizzaProjectApplicationTests {
 
